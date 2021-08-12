bb84029bda3d4b6d4b7fd8f565f41a956d72460a
hiromu.saito <hiromu.saito@bigtreetc.com>
2021-08-12 09:12:16 +0900

    ABC_085_B

---
+++ first_10_problems/ABC_085_B/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println(list.stream().distinct().count());
+    }
+}
